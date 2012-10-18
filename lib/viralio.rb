83e17209c4e7fc5ca4db2fd3b6dc423708eba7bb
relative <lamprou@live.com>
2012-10-18 06:17:55 +0300

    init

---
+++ lib/viralio.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'rest_client'
+require 'json'
+
+module Viralio
+	API_URL_subscription  = 'http://viralio.herokuapp.com/api/subscribe'
+	API_URL_progress  = 'http://viralio.herokuapp.com/api/progress'
+ 	API_VERSION = '0.0.1'
+
+ 	def self.new(api_key)
+ 		Viralio::Client.new(api_key)
+ 	end
+ 	
+ 	class Client 		
+ 		def initialize(api_key)
+	    	@api_key = api_key
+	    end
+
+	    def subscribe(mail, from)
+	    	subscription = RestClient.post API_URL_subscription, {:key => @api_key, :mail => mail, :from => from}
+	    	results = JSON.parse(subscription.to_str)
+	    	if results['status'] == 200
+	    		share = results['share']
+	    	else
+	    		error = results['error']	
+	    	end	
+	    end
+
+	    def progress(share)
+	    	progress = RestClient.post API_URL_progress, { :api_key => @api_key, :share => share }
+	    	results = JSON.parse(progress.to_str)
+	    	progress = results['progress']
+	    end
+ 	end
+end
---
+++ viralio.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'viralio'
+  s.version     = '0.0.2'
+  s.date = "2012-08-13"
+  s.summary     = "Get viral!"
+  s.description = "A simple gem"
+  s.authors     = ["Konstantinos Lamprou"]
+  s.email       = 'lamprou@live.com'
+  s.files       = ["lib/viralio.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/viralio'
+end
